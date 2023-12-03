7ac9246eb0e78f41bb878802d08c33003599b18d
Adam Chalmers <adam.s.chalmers@gmail.com>
2023-12-03 16:13:19 -0600

    Day 1 in C++

---
+++ day1-cpp/main.cpp
@@ -0,0 +1,76 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int numFromDigits(std::vector<int> digits) {
+    auto firstDigit = digits[0];
+    auto secondDigit = digits[digits.size()-1];
+    return firstDigit * 10 + secondDigit;
+}
+
+int parser1(std::string line) {
+    std::vector<int> digits = {};
+    for (auto curr : line) { 
+        if (curr >= '0' && curr <= '9') {
+            digits.push_back(curr-'0');
+        }
+    } 
+    
+    return numFromDigits(digits);
+}
+
+int parser2(std::string line) {
+    std::vector<std::tuple<std::string, int>> numerals = {
+        {"zero", 0},
+        {"one", 1},
+        {"two", 2},
+        {"three", 3},
+        {"four", 4},
+        {"five", 5},
+        {"six", 6},
+        {"seven", 7},
+        {"eight", 8},
+        {"nine", 9},
+        {"0", 0},
+        {"1", 1},
+        {"2", 2},
+        {"3", 3},
+        {"4", 4},
+        {"5", 5},
+        {"6", 6},
+        {"7", 7},
+        {"8", 8},
+        {"9", 9},
+    };
+
+    std::vector<int> digits = {};
+    for (auto i = 0; i < line.length(); i++) { 
+        auto slice = line.substr(i, line.size()-1);
+
+        for (auto numeral : numerals) {
+            auto spelling = std::get<0>(numeral);
+            auto value = std::get<1>(numeral);
+            if (slice.rfind(spelling, 0) == 0) {
+                digits.push_back(value);
+                break;
+            }
+        }
+    } 
+    
+    return numFromDigits(digits);
+}
+
+int main() {
+    std::ifstream file("../day1/input.txt");
+    std::string str; 
+    auto sumQ1 = 0;
+    auto sumQ2 = 0;
+    while (std::getline(file, str))
+    {
+        sumQ1 += parser1(str);
+        sumQ2 += parser2(str);
+    }
+    std::cout << "Q1: " << sumQ1 << std::endl;
+    std::cout << "Q2: " << sumQ2 << std::endl;
+}
