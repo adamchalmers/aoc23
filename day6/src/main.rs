ad208540cdc4e314658073d501dec1191d6da1df
Adam Chalmers <adam.s.chalmers@gmail.com>
2023-12-05 23:31:57 -0600

    Use rayon to take runtime from 15ms to 4ms

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,27 +1,45 @@
+use rayon::prelude::*;
+
 fn main() {
     let input = include_str!("../input.txt");
-    let record = parse(input);
-    let q1: usize = record.iter().map(Race::ways_to_beat).product();
+    let q1: usize = parse_q1(input).iter().map(Race::ways_to_beat).product();
     println!("Q1: {q1}");
     let q2 = parse_q2(input).ways_to_beat();
     println!("Q2: {q2}");
 }
 
-fn parse_line(s: &str) -> Vec<usize> {
-    s.split_once(':')
-        .unwrap()
-        .1
-        .trim()
-        .split_ascii_whitespace()
-        .map(|t| t.parse().unwrap())
-        .collect()
+#[derive(Debug)]
+struct Race {
+    duration: u64,
+    best_distance: u64,
 }
 
-fn parse(s: &str) -> Vec<Race> {
+impl Race {
+    fn ways_to_beat(&self) -> usize {
+        (1u64..self.duration)
+            .into_par_iter()
+            .filter(|acceleration| {
+                let time_remaining = self.duration - acceleration;
+                let distance_travelled = acceleration * time_remaining;
+                distance_travelled > self.best_distance
+            })
+            .count()
+    }
+}
+
+fn parse_q1(s: &str) -> Vec<Race> {
+    fn parse_line(s: &str) -> Vec<u64> {
+        s.split_once(':')
+            .unwrap()
+            .1
+            .trim()
+            .split_ascii_whitespace()
+            .map(|t| t.parse().unwrap())
+            .collect()
+    }
     let mut lines = s.lines();
     let times = parse_line(lines.next().unwrap());
     let distances = parse_line(lines.next().unwrap());
-
     times
         .into_iter()
         .zip(distances)
@@ -32,38 +50,19 @@ fn parse(s: &str) -> Vec<Race> {
         .collect()
 }
 
-#[derive(Copy, Clone, Debug)]
-struct Race {
-    duration: usize,
-    best_distance: usize,
-}
-
 fn parse_q2(s: &str) -> Race {
     let mut lines = s.lines();
     let time = lines.next().unwrap().split_once(':').unwrap().1.trim();
     let dist = lines.next().unwrap().split_once(':').unwrap().1.trim();
+    fn join_bad_kerning(s: &str) -> u64 {
+        s.chars()
+            .filter(char::is_ascii_digit)
+            .collect::<String>()
+            .parse()
+            .unwrap()
+    }
     Race {
         best_distance: join_bad_kerning(dist),
         duration: join_bad_kerning(time),
-    }
-}
-
-fn join_bad_kerning(s: &str) -> usize {
-    s.chars()
-        .filter(char::is_ascii_digit)
-        .collect::<String>()
-        .parse()
-        .unwrap()
-}
-
-impl Race {
-    fn ways_to_beat(&self) -> usize {
-        (1..self.duration)
-            .filter(|acceleration| {
-                let time_remaining = self.duration - acceleration;
-                let distance_travelled = acceleration * time_remaining;
-                distance_travelled > self.best_distance
-            })
-            .count()
     }
 }
