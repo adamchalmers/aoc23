097eb8152782e68456eea14725a0289f2f6b9a6b
Adam Chalmers <adam.s.chalmers@gmail.com>
2023-12-13 22:35:58 -0600

    Solve part 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,26 +1,13 @@
 fn main() {
     let grids = parse_grids(include_str!("../input.txt"));
-    // let grids = parse_grids(include_str!("../example2.txt"));
-    eprintln!("Loaded {} grids", grids.len());
     let q1: usize = grids
         .iter()
-        .map(|grid| match Reflection::of_q1(grid).unwrap() {
-            Reflection::Col(x) => x,
-            Reflection::Row(x) => 100 * x,
-        })
+        .map(|grid| Reflection::of_q1(grid, None).unwrap().score())
         .sum();
     println!("Q1: {q1}");
     let q2: usize = grids
         .into_iter()
-        .enumerate()
-        .map(|(i, grid)| {
-            let (answer, x, y) = Reflection::of_q2(i, grid);
-            println!("Smudge at ({x},{y})");
-            match answer {
-                Reflection::Col(x) => x,
-                Reflection::Row(x) => 100 * x,
-            }
-        })
+        .map(|grid| Reflection::of_q2(grid).score())
         .sum();
     println!("Q2: {q2}");
 }
@@ -61,14 +48,14 @@ impl Grid {
 }
 
 impl Reflection {
-    fn of_q1(grid: &Grid) -> Option<Self> {
+    fn of_q1(grid: &Grid, excluding: Option<&Reflection>) -> Option<Self> {
         // Try every row
         let height = grid.height();
         for y in 0..height - 1 {
             let mut rows_different = false;
             let mut a = y as isize;
             let mut b = y as isize + 1;
-            while b < (height as isize) && a >= 0 {
+            while a >= 0 && b < (height as isize) {
                 if grid.row(a as usize) != grid.row(b as usize) {
                     rows_different = true;
                     break;
@@ -76,8 +63,8 @@ impl Reflection {
                 a -= 1;
                 b += 1;
             }
-            if !rows_different {
-                return Some(Self::Row(y + 1));
+            if excluding != Some(&Reflection::Row(y)) && !rows_different {
+                return Some(Self::Row(y));
             }
         }
 
@@ -87,7 +74,7 @@ impl Reflection {
             let mut cols_different = false;
             let mut a = x as isize;
             let mut b = x as isize + 1;
-            while b < (width as isize) && a >= 0 {
+            while a >= 0 && b < (width as isize) {
                 if grid.col(a as usize) != grid.col(b as usize) {
                     cols_different = true;
                     break;
@@ -95,25 +82,26 @@ impl Reflection {
                 a -= 1;
                 b += 1;
             }
-            if !cols_different {
-                return Some(Self::Col(x + 1));
+            if excluding != Some(&Reflection::Col(x)) && !cols_different {
+                return Some(Self::Col(x));
             }
         }
         None
     }
 
-    fn of_q2(i: usize, mut grid: Grid) -> (Self, usize, usize) {
+    fn of_q2(mut grid: Grid) -> Self {
         // Answer cannot be this.
-        let answer_q1 = Self::of_q1(&grid).unwrap();
+        let answer_q1 = Self::of_q1(&grid, None).unwrap();
+        // Try smudging every cell.
         for x in 0..grid.width() {
             for y in 0..grid.height() {
-                // Smudge the grid.
+                // Smudge the grid at this cell.
                 grid.smudge(x, y);
 
                 // Was this smudge right?
-                match Self::of_q1(&grid) {
-                    Some(answer) if answer != answer_q1 => {
-                        return (answer, x + 1, y + 1);
+                match Self::of_q1(&grid, Some(&answer_q1)) {
+                    Some(answer) => {
+                        return answer;
                     }
                     _ => {
                         // If not, unsmudge it.
@@ -122,7 +110,14 @@ impl Reflection {
                 }
             }
         }
-        panic!("Failed to find any new line of reflection in grid {i}");
+        panic!("Failed to find any new line of reflection in grid");
+    }
+
+    fn score(&self) -> usize {
+        match self {
+            Reflection::Col(x) => x + 1,
+            Reflection::Row(x) => 100 * (x + 1),
+        }
     }
 }
 
@@ -133,11 +128,11 @@ mod tests {
     #[test]
     fn test_name() {
         for (expected, input) in [
-            (Reflection::Col(5), include_str!("../example.txt")),
-            (Reflection::Row(4), include_str!("../example2.txt")),
+            (Reflection::Col(4), include_str!("../example.txt")),
+            (Reflection::Row(3), include_str!("../example2.txt")),
         ] {
             let map = Grid::parse(input);
-            let actual = Reflection::of_q1(&map).unwrap();
+            let actual = Reflection::of_q1(&map, None).unwrap();
             assert_eq!(actual, expected);
         }
     }
