636f9edbcded4da9a35b5fbb36aa4c38bcda83eb
Adam Chalmers <adam.s.chalmers@gmail.com>
2023-12-13 22:19:58 -0600

    Start part 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,15 +1,28 @@
 fn main() {
     let grids = parse_grids(include_str!("../input.txt"));
-    // let grids = parse_grids(include_str!("../example_combined.txt"));
+    // let grids = parse_grids(include_str!("../example2.txt"));
     eprintln!("Loaded {} grids", grids.len());
     let q1: usize = grids
-        .into_iter()
-        .map(|grid| match Reflection::of(grid) {
+        .iter()
+        .map(|grid| match Reflection::of_q1(grid).unwrap() {
             Reflection::Col(x) => x,
             Reflection::Row(x) => 100 * x,
         })
         .sum();
     println!("Q1: {q1}");
+    let q2: usize = grids
+        .into_iter()
+        .enumerate()
+        .map(|(i, grid)| {
+            let (answer, x, y) = Reflection::of_q2(i, grid);
+            println!("Smudge at ({x},{y})");
+            match answer {
+                Reflection::Col(x) => x,
+                Reflection::Row(x) => 100 * x,
+            }
+        })
+        .sum();
+    println!("Q2: {q2}");
 }
 
 #[derive(Debug)]
@@ -41,44 +54,75 @@ impl Grid {
     fn height(&self) -> usize {
         self.0.len()
     }
+
+    fn smudge(&mut self, x: usize, y: usize) {
+        self.0[y][x] = if self.0[y][x] == '.' { '#' } else { '.' }
+    }
 }
 
 impl Reflection {
-    fn of(grid: Grid) -> Self {
+    fn of_q1(grid: &Grid) -> Option<Self> {
         // Try every row
         let height = grid.height();
         for y in 0..height - 1 {
-            let mut i = 0;
             let mut rows_different = false;
-            while y + 1 + i < height && (y as isize - i as isize >= 0) {
-                if grid.row(y - i) != grid.row(y + 1 + i) {
+            let mut a = y as isize;
+            let mut b = y as isize + 1;
+            while b < (height as isize) && a >= 0 {
+                if grid.row(a as usize) != grid.row(b as usize) {
                     rows_different = true;
                     break;
                 }
-                i += 1;
+                a -= 1;
+                b += 1;
             }
             if !rows_different {
-                return Self::Row(y + 1);
+                return Some(Self::Row(y + 1));
             }
         }
 
         // Try every column
         let width = grid.width();
         for x in 0..width - 1 {
-            let mut i = 0;
             let mut cols_different = false;
-            while x + i + 1 < width && (x as isize - i as isize >= 0) {
-                if grid.col(x - i) != grid.col(x + 1 + i) {
+            let mut a = x as isize;
+            let mut b = x as isize + 1;
+            while b < (width as isize) && a >= 0 {
+                if grid.col(a as usize) != grid.col(b as usize) {
                     cols_different = true;
                     break;
                 }
-                i += 1;
+                a -= 1;
+                b += 1;
             }
             if !cols_different {
-                return Self::Col(x + 1);
+                return Some(Self::Col(x + 1));
             }
         }
-        unreachable!()
+        None
+    }
+
+    fn of_q2(i: usize, mut grid: Grid) -> (Self, usize, usize) {
+        // Answer cannot be this.
+        let answer_q1 = Self::of_q1(&grid).unwrap();
+        for x in 0..grid.width() {
+            for y in 0..grid.height() {
+                // Smudge the grid.
+                grid.smudge(x, y);
+
+                // Was this smudge right?
+                match Self::of_q1(&grid) {
+                    Some(answer) if answer != answer_q1 => {
+                        return (answer, x + 1, y + 1);
+                    }
+                    _ => {
+                        // If not, unsmudge it.
+                        grid.smudge(x, y);
+                    }
+                }
+            }
+        }
+        panic!("Failed to find any new line of reflection in grid {i}");
     }
 }
 
@@ -93,7 +137,7 @@ mod tests {
             (Reflection::Row(4), include_str!("../example2.txt")),
         ] {
             let map = Grid::parse(input);
-            let actual = Reflection::of(map);
+            let actual = Reflection::of_q1(&map).unwrap();
             assert_eq!(actual, expected);
         }
     }
