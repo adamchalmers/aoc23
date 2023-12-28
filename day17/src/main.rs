9486e5d1cd42b722c56e4112408a22dab5868b59
Adam Chalmers <adam.s.chalmers@gmail.com>
2023-12-27 17:09:43 -0700

    Refactors

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,12 +1,11 @@
 use itertools::Itertools;
 use petgraph::{algo::dijkstra, graph::NodeIndex, Directed, Graph};
 
-const MAX_MOVES_IN_STRAIGHT_LINE: u8 = 3;
+const Q1_MAX_MOVES_IN_STRAIGHT_LINE: u8 = 3;
 
 fn main() {
     let grid = Grid::parse(include_str!("../input.txt"));
-    let a1 = grid.q1();
-    println!("Q1: {a1}");
+    println!("Q1: {}", grid.q1());
 }
 
 #[derive(Debug)]
@@ -44,51 +43,30 @@ impl Grid {
     }
 
     fn q1(&self) -> u32 {
-        let (graph, starts, ends) = self.build_graph(MAX_MOVES_IN_STRAIGHT_LINE, Node::edges_q1);
-        let distances_for_each_start: Vec<_> = starts
-            .iter()
-            .map(|start| dijkstra(&graph, *start, None, |e| *e.weight()))
-            .collect();
-        distances_for_each_start
-            .iter()
-            .map(|distances| {
-                let x: u32 = distances
-                    .iter()
-                    .filter_map(|(node_id, cost)| {
-                        if ends.contains(node_id) {
-                            Some(*cost)
-                        } else {
-                            None
-                        }
-                    })
-                    .min()
-                    .unwrap();
-                x
-            })
-            .min()
-            .unwrap()
+        let (graph, starts, ends) = self.build_graph(Node::edges_q1, Q1_MAX_MOVES_IN_STRAIGHT_LINE);
+        solve(graph, starts, ends)
     }
 
     /// Returns the graph, starts, and ends.
     fn build_graph<F>(
         &self,
-        max_turns: u8,
         has_edge: F,
+        max_moves_in_straight_line: u8,
     ) -> (Graph<Node, u32, Directed>, Vec<NodeIndex>, Vec<NodeIndex>)
     where
-        F: Fn(&Node, Node) -> bool,
+        F: Fn(&Node, Node, u8) -> bool,
     {
         let mut g = Graph::new();
         let nodes: Vec<(NodeIndex, Node)> = (0..self.width)
             .cartesian_product(0..self.height)
-            .cartesian_product(0u8..max_turns)
+            .cartesian_product(0u8..max_moves_in_straight_line)
             .cartesian_product([Dir::Up, Dir::Down, Dir::Left, Dir::Right])
             .map(|(((x, y), moves_since_turn), current_direction)| Node {
                 point: Point {
                     x: x as i32,
                     y: y as i32,
                 },
-                moves_since_turn,
+                moves_in_straight_line: moves_since_turn,
                 current_direction,
             })
             .map(|node| {
@@ -110,9 +88,8 @@ impl Grid {
                 ends.push(*i);
             }
             for (j, node1) in &nodes {
-                if has_edge(node0, *node1) {
+                if has_edge(node0, *node1, max_moves_in_straight_line) {
                     g.add_edge(*i, *j, self.at(node1.point).unwrap());
-                    // eprintln!("Edge from {node0:?} to {node1:?}")
                 }
             }
         }
@@ -120,10 +97,35 @@ impl Grid {
     }
 }
 
+fn solve(graph: Graph<Node, u32>, starts: Vec<NodeIndex>, ends: Vec<NodeIndex>) -> u32 {
+    let distances_for_each_start: Vec<_> = starts
+        .iter()
+        .map(|start| dijkstra(&graph, *start, None, |e| *e.weight()))
+        .collect();
+    distances_for_each_start
+        .iter()
+        .map(|distances| {
+            let x: u32 = distances
+                .iter()
+                .filter_map(|(node_id, cost)| {
+                    if ends.contains(node_id) {
+                        Some(*cost)
+                    } else {
+                        None
+                    }
+                })
+                .min()
+                .unwrap();
+            x
+        })
+        .min()
+        .unwrap()
+}
+
 #[derive(Clone, Copy, Hash, Eq, PartialEq)]
 struct Node {
     point: Point,
-    moves_since_turn: u8,
+    moves_in_straight_line: u8,
     current_direction: Dir,
 }
 
@@ -132,13 +134,14 @@ impl std::fmt::Debug for Node {
         write!(
             f,
             "{:?} {:?} ({})",
-            self.point, self.current_direction, self.moves_since_turn
+            self.point, self.current_direction, self.moves_in_straight_line
         )
     }
 }
 
 impl Node {
-    fn edges_q1(&self, next: Node) -> bool {
+    /// Returns true if there's a directed edge from self to next in the graph for Q1.
+    fn edges_q1(&self, next: Node, max_moves_in_straight_line: u8) -> bool {
         match (self.current_direction, next.current_direction) {
             // Same direction
             (Dir::Up, Dir::Up)
@@ -146,15 +149,15 @@ impl Node {
             | (Dir::Left, Dir::Left)
             | (Dir::Right, Dir::Right) => {
                 // Cannot go in the same direction forever.
-                if self.moves_since_turn >= MAX_MOVES_IN_STRAIGHT_LINE {
+                if self.moves_in_straight_line > max_moves_in_straight_line {
                     return false;
                 }
                 // The next step must be 1 step forwards.
-                if self.moves_since_turn + 1 != next.moves_since_turn {
+                if self.moves_in_straight_line + 1 != next.moves_in_straight_line {
                     return false;
                 }
                 // The next location must actually be 1 step along the current direction.
-                
+
                 match self.current_direction {
                     Dir::Up => self.point.x == next.point.x && self.point.y - 1 == next.point.y,
                     Dir::Down => self.point.x == next.point.x && self.point.y + 1 == next.point.y,
@@ -169,7 +172,7 @@ impl Node {
             | (Dir::Right, Dir::Left) => false,
             // Remaining cases are all turns.
             turn => {
-                if next.moves_since_turn != 0 {
+                if next.moves_in_straight_line != 0 {
                     return false;
                 }
                 match turn {
