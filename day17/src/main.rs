4dbe4c192c6f915a3c2ff2bf8de2653ce6380e21
Adam Chalmers <adam.s.chalmers@gmail.com>
2024-01-15 00:28:34 -0600

    Finish day 17

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 fn main() {
     let grid = Grid::parse(include_str!("../input.txt"));
     println!("Q1: {}", grid.q1());
+    println!("Q2: {}", grid.q2());
 }
 
 #[derive(Debug)]
@@ -64,7 +65,63 @@ impl Grid {
             if solutions.len() >= 2 {
                 return solutions.values().copied().min().unwrap();
             }
-            for neighbour in curr.neighbours(self.width, self.height) {
+            for neighbour in curr.neighbours_q1(self.width, self.height) {
+                // Don't visit the same node twice.
+                if visited.contains_key(&neighbour) {
+                    continue;
+                }
+
+                let cost_through_here = cost + self.at(neighbour.point);
+                let min_cost = if let Some(previous_cost) =
+                    tentative.get_priority(&neighbour).map(|p| p.cost)
+                {
+                    cost_through_here.min(previous_cost)
+                } else {
+                    cost_through_here
+                };
+                tentative.push(neighbour, Priority { cost: min_cost });
+            }
+            visited.insert(curr, cost);
+        }
+        panic!("Finished all tentative nodes but never found a terminal node")
+    }
+
+    fn q2(&self) -> u32 {
+        let mut solutions = HashMap::new();
+        let mut visited: HashMap<Node, u32> = HashMap::new();
+        let mut tentative = PriorityQueue::new();
+        tentative.push(
+            Node {
+                point: Default::default(),
+                moves_in_straight_line: 0,
+                current_direction: Dir::Down,
+            },
+            Priority { cost: 0 },
+        );
+        tentative.push(
+            Node {
+                point: Default::default(),
+                moves_in_straight_line: 0,
+                current_direction: Dir::Right,
+            },
+            Priority { cost: 0 },
+        );
+        // Get highest-priority item
+        while let Some((curr, priority)) = tentative.pop() {
+            let cost = priority.cost;
+            // Are we at the final node?
+            if curr.moves_in_straight_line >= 4
+                && curr.point.x == self.width - 1
+                && curr.point.y == self.height - 1
+            {
+                solutions.insert(curr, cost);
+            }
+            // You can enter the final node from above or from left.
+            // Once we've checked both of them, exit.
+            if solutions.len() >= 2 {
+                return solutions.values().copied().min().unwrap();
+            }
+            for neighbour in curr.neighbours_q2(self.width, self.height) {
                 // Don't visit the same node twice.
                 if visited.contains_key(&neighbour) {
                     continue;
@@ -125,8 +182,7 @@ impl std::fmt::Debug for Node {
 }
 
 impl Node {
-    /// Returns true if there's a directed edge from self to next in the graph for Q1.
-    fn neighbours(&self, width: usize, height: usize) -> Vec<Self> {
+    fn neighbours_q1(&self, width: usize, height: usize) -> Vec<Self> {
         const MAX_MOVES_IN_SAME_DIR: u8 = 3;
         [Dir::Up, Dir::Down, Dir::Left, Dir::Right]
             .into_iter()
@@ -151,6 +207,42 @@ impl Node {
                         | (Dir::Right, Dir::Left) => return None,
                         // Remaining cases are all turns.
                         (_, next_direction) => (next_direction, 1),
+                    };
+                Node::move_along(self.point, next_dir, moves_in_straight_line, width, height)
+            })
+            .collect()
+    }
+    fn neighbours_q2(&self, width: usize, height: usize) -> Vec<Self> {
+        const MAX_MOVES_IN_SAME_DIR: u8 = 10;
+        const MIN_MOVES_BEFORE_TURNING: u8 = 4;
+        [Dir::Up, Dir::Down, Dir::Left, Dir::Right]
+            .into_iter()
+            .filter_map(|next_direction| {
+                let (next_dir, moves_in_straight_line) =
+                    match (self.current_direction, next_direction) {
+                        // Same direction
+                        (Dir::Up, Dir::Up)
+                        | (Dir::Down, Dir::Down)
+                        | (Dir::Left, Dir::Left)
+                        | (Dir::Right, Dir::Right) => {
+                            // Cannot go in the same direction forever.
+                            if self.moves_in_straight_line >= MAX_MOVES_IN_SAME_DIR {
+                                return None;
+                            }
+                            (self.current_direction, self.moves_in_straight_line + 1)
+                        }
+                        // Cannot go backwards.
+                        (Dir::Up, Dir::Down)
+                        | (Dir::Down, Dir::Up)
+                        | (Dir::Left, Dir::Right)
+                        | (Dir::Right, Dir::Left) => return None,
+                        // Remaining cases are all turns.
+                        (_, next_direction) => {
+                            if self.moves_in_straight_line < MIN_MOVES_BEFORE_TURNING {
+                                return None;
+                            }
+                            (next_direction, 1)
+                        }
                     };
                 Node::move_along(self.point, next_dir, moves_in_straight_line, width, height)
             })
